69a688d3be808bff825170e04bc5dbe1d1327dca
viviannemelo <viviannemmelo@gmail.com>
2022-12-14 18:50:47 -0300

    Expenses Form (req 3)

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import WalletForm from '../components/WalletForm';
 
 class Wallet extends React.Component {
   render() {
     return (
       <div>
         <Header />
-        <h1>Wallet</h1>
+        <WalletForm />
       </div>
     );
   }
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,12 @@
 export const EMAIL_USER = 'EMAIL_USER';
+export const WALLET_ACTION = 'WALLET_ACTION';
 
 export const userAction = (email) => ({
   type: EMAIL_USER,
   payload: email,
+});
+
+export const walletAction = (currencies) => ({
+  type: WALLET_ACTION,
+  payload: currencies,
 });
