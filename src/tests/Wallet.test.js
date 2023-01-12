3cc3f11a0b40cb51522d13b882b3f41d548c47e6
viviannemelo <viviannemmelo@gmail.com>
2023-01-11 23:50:43 -0300

    Wallet test

--- src/tests/Wallet.test.js
+++ src/tests/Wallet.test.js
@@ -1,9 +1,37 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
-import Wallet from '../pages/Wallet';
+import App from '../App';
 
-describe('', () => {
-  it('', () => {
+const EMAIL_INPUT = 'email-input';
+const PASSWORD_INPUT = 'password-input';
 
+describe('Wallet page tests', () => {
+  it('If contains all the elements', () => {
+    renderWithRouterAndRedux(<App />);
+
+    const emailInsertion = screen.getByTestId(EMAIL_INPUT);
+    const passwordInsertion = screen.getByTestId(PASSWORD_INPUT);
+    const loginButton = screen.getByRole('button', { name: /entrar/i });
+
+    userEvent.type(emailInsertion, 'example@example.com');
+    userEvent.type(passwordInsertion, '123456');
+    userEvent.click(loginButton);
+
+    const emailField = screen.getByTestId('email-field');
+    const totalField = screen.getByTestId('total-field');
+    const currencyField = screen.getByTestId('header-currency-field');
+    const valueInput = screen.getByTestId('value-input');
+    const descripionInput = screen.getByTestId('description-input');
+    const methodInput = screen.getByTestId('method-input');
+    const tagInput = screen.getByTestId('tag-input');
+
+    expect(emailField.textContent).toBe('example@example.com');
+    expect(totalField.textContent).toBe('0.00');
+    expect(currencyField.textContent).toBe('BRL');
+    expect(valueInput).toBeInTheDocument();
+    expect(descripionInput).toBeInTheDocument();
+    expect(methodInput).toBeInTheDocument();
+    expect(tagInput).toBeInTheDocument();
   });
 });
